9342df94b5ffcd68dcfa328cdd11f884f3ef3261
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-03-31 20:02:11 +0200

    new stuff

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect } from 'react'
-import { PerspectiveCamera, OrbitControls, Environment } from '@react-three/drei'
+import { PerspectiveCamera, OrbitControls, Environment, Grid, Circle, Ring } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { angleToRadius } from '../../utils/angle'
 import * as THREE from 'three'
+
 import gsap from 'gsap'
+import { Vector3 } from 'three'
 
 const Three = () => {
   // code to move camera around
@@ -19,6 +21,7 @@ const Three = () => {
 
   // Animation
   const ballRef = useRef(null)
+
   useEffect(() => {
     if (!!ballRef.current) {
       console.log(ballRef.current)
@@ -75,20 +78,72 @@ const Three = () => {
     }
   }, [ballRef])
 
+  // const ringRef = useRef(null)
+
+  // const initBalls = () => {
+  //   let ballGeom = new THREE.SphereGeometry(1, 16, 16)
+
+  //   for (let i = 1; i <= 30; i++) {
+  //     createBall(ballGeom, Math.random() * 0xffffff, 0, 1, 0)
+  //   }
+  // }
+
+  // // Ball making function
+
+  // const createBall = (ballGeom, color, x, y, z) => {
+  //   let ball = new THREE.Mesh(ballGeom, new THREE.MeshStandardMaterial({ color }))
+  //   console.log(ball.position)
+  //   ball.position.set(x, y, z)
+  //   ball.userData.vel = new THREE.Vector3().randomDirection()
+  //   ball.userData.vel.multiplyScalar(2)
+  // }
+
+  // useEffect(() => {
+  //   initBalls()
+  // }, [])
+
   return (
     <>
-      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-      <OrbitControls ref={orbitControlsRef} minPolarAngle={angleToRadius(60)} maxPolarAngle={angleToRadius(80)} />
+      {/* Grid */}
+      <Grid
+        position={[0, 0, 0]}
+        args={[15, 15]}
+        sectionSize={5}
+        sectionColor="#39b6ba"
+        sectionThickness={1.1}
+        cellSize={0.14}
+        cellColor="grey"
+        cellThickness={0.7}
+        fadeDistance={2.5}
+        fadeStrength={1.6}
+        followCamera={false}
+      />
+
+      <PerspectiveCamera makeDefault position={[0, 4, 0]} />
+      <OrbitControls makeDefault />
+
+      {/* Circle */}
+      {/* <mesh rotation={[-angleToRadius(90), 0, 0]}>
+        <Circle args={[1.5, 128]} />
+      </mesh> */}
+
+      {/* Ring */}
+      <mesh rotation={[-angleToRadius(90), 0, 0]}>
+        <sphereGeometry args={[1, 32, 32, 0]} position={[10, 1, 0]} />
+
+        <meshStandardMaterial />
+      </mesh>
+
       {/* Ball */}
-      <mesh position={[-2, 1.5, 0]} castShadow ref={ballRef}>
+      {/* <mesh position={[-2, 1.5, 0]} castShadow ref={ballRef}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="white" metalness={0.6} roughness={0.2} />
-      </mesh>
+      </mesh> */}
       {/* Floor */}
-      <mesh rotation={[-angleToRadius(90), 0, 0]} receiveShadow>
-        <planeGeometry args={[20, 20]} />
-        <meshPhongMaterial attach="material" color="#2596be" />
-      </mesh>
+      {/* <mesh rotation={[-angleToRadius(90), 0, 0]} receiveShadow>
+        <planeGeometry args={[2, 2]} />
+        <meshStandardMaterial attach="material" color="#2596be" />
+      </mesh> */}
       {/* Ambient light */}
       <ambientLight args={['#ffffff', 0.1]} />
 
@@ -96,12 +151,12 @@ const Three = () => {
       <spotLight args={['#ffffff', 1.5, 7, angleToRadius(45), 0.4]} position={[-3, 1, 0]} castShadow />
 
       {/* Environment */}
-      <Environment background>
+      {/* <Environment background>
         <mesh>
           <sphereGeometry args={[50, 100, 100]} />
-          <meshBasicMaterial side={THREE.BackSide} color="#abdbe3" />
+          <meshBasicMaterial side={THREE.BackSide} color="black" />
         </mesh>
-      </Environment>
+      </Environment> */}
     </>
   )
 }
